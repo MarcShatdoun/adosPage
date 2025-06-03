5c7b70f1a8fec20e259a1ea7e8cdce16dfc2f2c2
Marc <marc.quimis@gmail.com>
2025-06-03 10:29:45 +0200

    add json data img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <>
         <h1 className='text-2xl text-center'>Quien es ado?</h1>
         <div className="w-full h-auto my-2 bg-[#1B1B1D] flex justify-center items-center">
-            <img className='' src="./src/assets/img/adopic.webp" alt="" />
+            <img className='rounded-lg' src="./src/assets/img/adopic.webp" alt="" />
             <div>
                 <h2>Ado</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quod ad consequuntur aut dicta totam iure nobis natus aliquam eum.</p>
